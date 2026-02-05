59d2a2cbc20a29c9b1c7fc6c842e6a3a8dd91343
Oliver Ekeno <165812391+OLIVER-LOMANAT@users.noreply.github.com>
2026-02-05 14:47:25 +0300

    Update ChatContainer.jsx

--- frontend/src/components/ChatContainer.jsx
+++ frontend/src/components/ChatContainer.jsx
@@ -66,7 +66,7 @@ const ChatContainer = () => {
                 />
               </div>
             </div>
-            <div className="chat-header mb-1">
+            <div className="chat-footer mb-1">
               <time className="text-xs opacity-50 ml-1">
                 {formatMessageTime(message.createdAt)}
               </time>
